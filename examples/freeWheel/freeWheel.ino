af953e3e966d997466dce26fb8b82f190a5688cf
DiogoValdrez <diogovaldrez02@gmail.com>
2024-07-25 11:39:38 +0100

    fix(examples): fixed buggs in frewheel example, now it also stops spending energy when stopped.

--- examples/freeWheel/freeWheel.ino
+++ examples/freeWheel/freeWheel.ino
@@ -1,8 +1,8 @@
 /*
   Name:    freeWheel.ino
-  Modified: 20-06-2024
+  Modified: 25-07-2024
   By:  DiogoValdrez
-  Description: This is a very simple example of a possible freewheel implementarion in a esp32c3 dev M1.
+  Description: This is a very simple example of a possible freewheel implementation in a esp32c3 dev M1.
 */
 
 #include <ComEVesc.h>
@@ -19,7 +19,7 @@ bool stopped = false;
 
 void setup() {
   Serial.begin(9600);
-  VescSerial.begin(115200, SERIAL_8N1, 9, 10); // 19200 baud, 8 data bits, no parity, 1 stop bit, pins 9 (RX) and 10 (TX)
+  VescSerial.begin(115200, SERIAL_8N1, 9, 10); // 115200 baud, 8 data bits, no parity, 1 stop bit, pins 9 (RX) and 10 (TX)
   
   while (!Serial) {;}
 
@@ -29,20 +29,34 @@ void setup() {
 
 void loop() {
   /** Call the function setCurrent() to set the motor current */
-  delay(1000);
   Serial.println("Ramp up");
+  stopped = false;
+
+  
   unsigned long startTime = millis();
+
   while (millis() - startTime < rampTime) {
     UART.setCurrent(maxcurrent);
+    delay(500); 
+    UART.getVescValues();
+    Serial.println(UART.data.rpm);
+    delay(500); // Introduce a delay to prevent CPU overload
   }
-  Serial.println("Free wheel Start"); //TODO: check if speed is zero to stop spending energy, now bugging the esp and crashing it
+  Serial.println("Free wheel Start");
+  
   while(!stopped) {
     UART.getVescValues();
+    delay(500);
+
     if(UART.data.rpm == 0) {
       stopped = true;
+    } else {
+      UART.setCurrent(freecurrent);
     }
     Serial.println(UART.data.rpm);
-    UART.setCurrent(freecurrent);
+    delay(500); // Introduce a delay to prevent continuous high CPU load
   }
-  Serial.println("Free wheel Stoped");
+
+  Serial.println("Free wheel Stopped");
+  delay(50000); // Add a delay before the loop restarts
 }
